a9c7bf3e8b800740466bda089d52bc8814824abe
christian cecilia <christian.cecilia1@gmail.com>
2020-07-10 22:06:13 -0400

    Remote (#2)
    * keypress command and moved device discover and config reading into utils
    
    * fixed zipping issue
    
    * fixed deploy, starting debugger

--- pyku/__main__.py
+++ pyku/__main__.py
@@ -9,14 +9,19 @@ Commands:
     Flags:
         -c, --channel - Path to channel to be deployed, REQUIRED
         --skip-discovery - skip device discovery and use only device designated in config
+
+    keypress - simulates a remote keypress
+
+    Flags:
+        -b, --button - Button pressed, REQUIRED
+        -c, --channel - Path to channel to be deployed, REQUIRED
+        --skip-discovery - skip device discovery and use only device designated in config
 ToDos:
 """
 # standard lib imports
 from typing import Union
 # third party lib imports
 import click
-from PyInquirer import prompt
-from roku_scanner.scanner import Scanner
 # project imports
 from pyku.channel import Channel
 from pyku.roku import Roku
@@ -42,7 +47,8 @@ def cli():
     required=True
 )
 @click.option('--skip-discovery', 'skip_discovery', flag_value=True)
-def deploy(channel_path: str, skip_discovery: bool):
+@click.option('--debugger', 'debugger', flag_value=True)
+def deploy(channel_path: str, skip_discovery: bool, debugger: bool):
     """
     Deploy Command
     :param channel_path: Path to channel project's root dir
@@ -72,98 +78,56 @@ def deploy(channel_path: str, skip_discovery: bool):
     channel.stage_channel_for_compilation()
     click.echo('creating archive')
     channel.archive_staged_content_to_out()
-    rokus: list = []
     selected_devices: list = []
+    deploy_status: str = "false"
 
     if not skip_discovery:
-        # Discover devices
-        click.echo('discovering devices')
-        scanner: Scanner = Scanner()
-        scanner.discover()
-
-        for device in scanner.discovered_devices:
-            roku_location: str = device.get('LOCATION')
-            roku: Roku = Roku(location=roku_location, discovery_data=device)
-            roku.fetch_data()
-            rokus.append(roku)
-
-        if len(rokus) > 0:
-            # setup rokus for selection prompt
-            choices: list = [utils.serialize_roku_object_for_selection(roku) for roku in rokus if roku.developer_enabled]
-            device_selection_questions: list = [
-                {
-                    "type": 'checkbox',
-                    "message": 'Select Device(s)',
-                    "name": 'selected_devices',
-                    "choices": choices,
-                    'validate': lambda answer: 'You must choose at least one device.'
-                    if len(answer) == 0 else True
-                }
-            ]
-            selected_rokus: dict = prompt(device_selection_questions)
-            selected_devices.append(selected_rokus.get('selected_devices', []))
-
-            if len(selected_devices) > 0:
-                # check for roku dev password in config or prompt user for it
-                for selected in selected_devices:
-                    config_check: Union[None, dict] = utils.check_if_roku_exists_in_config(selected, channel)
-                    if config_check is not None:
-                        selected.password = config_check.get('password', None)
-                        selected.selected = True
-                        config_username: Union[str, None] = config_check.get('username', None)
-                        if config_username is not None:
-                            selected.user_name = config_username
-                    else:
-                        password_prompt: str = click.prompt(
-                            f'password for {selected.location}',
-                            prompt_suffix='? ',
-                            type=str
-                        )
-                        if password_prompt != '':
-                            selected.password = password_prompt
-                            selected.selected = True
-                        else:
-                            selected_devices.remove(selected)
-                            click.echo(f'unable to use {selected.friendly_model_name} @ {selected.location}')
-
-                    selected.deploy_archive(channel.channel_archive)
+        selected_devices = utils.run_device_discovery(channel)
     else:
-        # create roku objects from config
-        click.echo('reading config for rokus')
-        config_rokus: list = channel.channel_config.rokus
-        if not len(config_rokus):
-            click.echo('cannot skip device discovery without rokus designated in config')
-
-        for roku_config in config_rokus:
-            roku_config = utils.merge_roku_config_values_into_dict(roku_config)
-            click.echo(f'found config for {roku_config.get("ip_address", "")}')
-            roku: Roku = Roku(
-                location=f'http://{roku_config.get("ip_address", "")}:8060/',
-                discovery_data={
-                    'WAKEUP': '',
-                    'device-group.roku.com': '',
-                    'LOCATION': f'http://{roku_config.get("ip_address", "")}:8060/',
-                    'Server': 'Roku/9.3.0 UPnP/1.0 Roku/9.3.0',
-                    'Ext': '',
-                    'USN': '',
-                    'ST': 'roku:ecp',
-                    'Cache-Control': 'max-age=3600'
-                }
-            )
-            roku.fetch_data()
-            roku.selected = True
-            roku.password = roku_config.get("password", "")
-            config_username: Union[str, None] = roku_config.get('username', None)
-            if config_username is not None:
-                roku.user_name = config_username
-            selected_devices.append(roku)
+        selected_devices = utils.get_selected_from_config(channel)
 
     # loop through selected rokus and deploy archives
     if len(selected_devices) > 0:
         for selected in selected_devices:
             result_msgs: list = selected.deploy_archive(channel.channel_archive)
             for msg in result_msgs:
-                click.echo(f'{selected.friendly_model_name} | Status {msg["status"]} | {msg["msg"]}')
+                deploy_status = msg["status"]
+                click.echo(f'{selected.friendly_model_name} | Status {deploy_status} | {msg["msg"]}')
+
+    if deploy_status == "success" and debugger:
+        print("run debugger")
+
+
+@cli.command()
+@click.option(
+    '-b',
+    '--button',
+    help='Simulated button pressed',
+    type=str,
+    required=True
+)
+@click.option(
+    '-c',
+    '--channel',
+    'channel_path',
+    help='Path to channel project\'s root dir',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=False, readable=True),
+    required=False
+)
+@click.option('--skip-discovery', 'skip_discovery', flag_value=True)
+def keypress(button: str, channel_path: str, skip_discovery: bool):
+    click.echo('key press')
+    channel: Channel = Channel(channel_path)
+    selected_devices: list = []
+
+    if not skip_discovery:
+        selected_devices.append(utils.run_device_discovery(channel))
+    else:
+        selected_devices.append(utils.get_selected_from_config(channel))
+
+    if len(selected_devices) > 0:
+        for selected in selected_devices:
+            selected.send_remote_command(button)
 
 
 if __name__ == '__main__':
---
+++ tests/mockDevChannel/components/TestChannelScene.brs
@@ -0,0 +1,3 @@
+sub init()
+    ? "init"
+end sub
